f0d892c93b80c8265c35aa701f193652e2181801
akshay yadav <akshayyadav7613@gmail.com>
2019-06-08 17:07:14 +0530

    convert app into new desgin..

---
+++ lib/custom_icon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon{
+  static const IconData menu = IconData(0xe900,fontFamily: "CusomIcons");
+  static const IconData option = IconData(0xe902,fontFamily: "CusomIcons");
+
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:flare_flutter/flare_actor.dart';
 
-void main() => runApp(MyApp());
+import 'custom_icon.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      body:
-      Center(heightFactor: 200,widthFactor: 200,
-          child: Stack(children: <Widget>[
-            Container(height: 600,width: 300,color: Colors.amberAccent,),
-            Card(child: Text("this animeton"),color: Colors.blueAccent,),
-
-            FlareActor(
-              "anime/new_file.flr",
-              animation: "circular",
-              fit: BoxFit.contain,
-
-
-            )
-          ],)
-
-         ),
+      backgroundColor: Color(0xff2d3447),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Tranding",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36.0,
+                      fontFamily: "Calibre-Semibold",
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_drop_down,
+                      size: 12.0,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child:
+            Row(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),child: Center(
+                  child: Padding(padding: EdgeInsets.symmetric(horizontal: 22.0,vertical: 6.0),
+                  child: Text("Animated",style: TextStyle(color: Colors.white),),),
+                  
+                ),
+                )
+              ],
+            ),),
+          ],
+        ),
+      ),
     );
   }
 }
