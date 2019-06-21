cc9fb8d618e2174a18ea81733d1190c49dcfe45e
akshay yadav <akshayyadav7613@gmail.com>
2019-06-21 16:09:09 +0530

    fish app error expected and hopefully fond it

---
+++ lib/data.dart
@@ -0,0 +1,15 @@
+
+
+List<String>images=[
+    'images/Hy.jpg'
+    'images/Jellyfish.jpg'
+     'images/Koala.jpg'
+    'images/Light.jpg'
+];
+
+List<String>ImgaesTitle=[
+  'Hy'
+      'Jellyfish'
+      'Koala'
+      'Light'
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'custom_icon.dart';
+import './data.dart';
+import 'dart:math';
 
 main() {
   runApp(MaterialApp(
@@ -13,9 +15,21 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+var cardAspectRation = 12.0 / 16.0;
+var widegetAspectRation = cardAspectRation * 1.2;
+
 class _HomeState extends State<Home> {
+  var CurrentPage = images.length - 1.0;
+
   @override
   Widget build(BuildContext context) {
+    PageController controller = PageController(initialPage: images.length - 1);
+    controller.addListener(() {
+      setState(() {
+        CurrentPage = controller.page;
+      });
+    });
+
     return Scaffold(
       backgroundColor: Color(0xff2d3447),
       body: SingleChildScrollView(
@@ -73,26 +87,110 @@ class _HomeState extends State<Home> {
             ),
             Padding(
               padding: EdgeInsets.only(left: 20.0),
-              child:
-            Row(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.deepOrange,
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),child: Center(
-                  child: Padding(padding: EdgeInsets.symmetric(horizontal: 22.0,vertical: 6.0),
-                  child: Text("Animated",style: TextStyle(color: Colors.white),),),
-                  
-                ),
-                ),
-                SizedBox(width: 10.0,),
-                Text("30+ Stories",style: TextStyle(color: Colors.white),),
-              ],
-            ),),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.deepOrange,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 22.0, vertical: 6.0),
+                        child: Text(
+                          "Animated",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "30+ Stories",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Stack(
+                    children: <Widget>[
+                      CardScrollWidget(CurrentPage),
+                      Positioned.fill(
+                          child: PageView.builder(
+                              itemCount: images.length,
+                              controller: controller,
+                              reverse: true,
+                              itemBuilder: (context, index) {
+                                return Container();
+                              }))
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class CardScrollWidget extends StatelessWidget {
+  var currentpage;
+  var padding = 20.0;
+  var verticalInsert = 20.0;
+
+  CardScrollWidget(this.currentpage);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: widegetAspectRation,
+      child: LayoutBuilder(builder: (context, contraints) {
+        var width = contraints.maxWidth;
+        var height = contraints.maxHeight;
+
+        var safeWidget = width - 2 * padding;
+        var safeheight = height - 2 * padding;
+
+        var heightOfPrimaryCard = safeheight;
+        var widthtOfPrimaryCard = heightOfPrimaryCard * cardAspectRation;
+
+        var primaryCardLeft = safeWidget - widthtOfPrimaryCard;
+        var horizontalInsert = primaryCardLeft / 2;
+
+
+        List<Widget>cardList =List();
+        for (var i = 0; i < images.length; i++) {
+          var delete = i - currentpage;
+          bool isOnRight = delete > 0;
+          var start = padding +
+              max(
+                  primaryCardLeft -
+                      horizontalInsert * -delete * (isOnRight ? 15 : 1),
+                  0.0);
+          var cardItem = Positioned.directional(
+            top: padding + verticalInsert * max(-delete, 0.0),
+            bottom: padding + verticalInsert * max(-delete, 0.0),
+            start: padding + start,
+            textDirection: TextDirection.rtl,
+            child: Container(
+              child: AspectRatio(aspectRatio: cardAspectRation,
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Image.asset(images[i],fit: BoxFit.cover,)
+                ],
+              ),),
+              
+            ),
+          );
+          cardList.add(cardItem);
+        }
+        return Stack(
+          children: cardList,
+        );
+      }),
     );
   }
 }
