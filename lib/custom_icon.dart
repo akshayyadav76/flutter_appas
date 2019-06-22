0ae86a6a894d6c2a3139129e63b52cde125b5756
akshay yadav <akshayyadav7613@gmail.com>
2019-06-22 15:11:41 +0530

    begin to create the status app,.....

--- lib/custom_icon.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CustomIcon{
-  static const IconData menu = IconData(0xe900,fontFamily: "CusomIcons");
-  static const IconData option = IconData(0xe902,fontFamily: "CusomIcons");
-
-
-
-}
